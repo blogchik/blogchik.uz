bfb34d4a219681623f7c238ac6185113ba4cc3f9
blogchik <blogchikuz@gmail.com>
2023-06-17 17:01:31 -0700

    Footer Complete

---
+++ src/web/footer.php
@@ -0,0 +1,47 @@
+<footer class="footer">
+
+    <div class="footer__content">
+
+        <div class="side">
+
+            <ul class="footer__menu">
+
+                <li class="footer__menu_item">
+
+                    <a href="./about">About</a>
+
+                </li>
+
+                <li class="footer__menu_item">
+
+                    <a href="./projects">Projects</a>
+
+                </li>
+
+                <li class="footer__menu_item">
+
+                    <a href="./resume">Resume</a>
+
+                </li>
+
+                <li class="footer__menu_item">
+
+                    <a href="./gallery">Gallery</a>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+        <div class="side">
+
+            <a href="./" class="footer__logo">
+                <img src="./assets/images/logo-rgb.png" alt="Logo">
+            </a>
+
+        </div>
+
+    </div>
+
+</footer>
